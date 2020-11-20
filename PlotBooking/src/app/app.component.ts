cdff6693fea029fc5e9577dec07f37cc8be68b3c
himanshu14263 <himanshu14263@gmail.com>
2020-11-20 21:07:22 +0530

    connected with backend

--- PlotBooking/src/app/app.component.ts
+++ PlotBooking/src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import Sites from '../models/Sites';
 import Row from 'src/models/Row';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { PlotBookingServiceService } from './plot-booking-service.service';
+import Customer from 'src/models/Customer';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+// import { NzFormTooltipIcon } from 'ng-zorro-antd/form';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +12,59 @@ import Row from 'src/models/Row';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+
+  constructor(private fb: FormBuilder, private plotBookingService: PlotBookingServiceService,
+              private notification: NzNotificationService) {}
   title = 'PlotBooking';
   rows: Row[] = [];
   isVisible: boolean;
   modalTitle;
+  sites: boolean[] = [];
+  owner: Customer;
+
+  plot: number;
+
+  validateForm!: FormGroup;
 
   ngOnInit(): void {
+
+    for (let i = 0; i <= 48; i++) {this.sites.push(false); }
+
+    this.plotBookingService.getPeople().subscribe((data) => {
+      // tslint:disable-next-line: prefer-for-of
+      for (let i  = 0; i < data.length; i++) {
+        this.sites[data[i].id] = true;
+      }
+    });
+
+    console.log(this.sites);
     for (let i = 0; i < 8; i++) {
       const row = new Row(i);
       this.rows.push(row);
     }
     this.isVisible = false;
+
+    this.validateForm = this.fb.group({
+      email: [null, [Validators.email, Validators.required]],
+      name: [null, [Validators.required]],
+      phoneNumberPrefix: ['+91'],
+      phoneNumber: [null, [Validators.required]]
+    });
+  }
+
+  createNotification(type: string): void {
+    this.notification.create(
+      type,
+      'Notification Title',
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.'
+    );
   }
 
   showModal(plotNumber): void {
+    this.plot = plotNumber;
     this.isVisible = true;
     this.modalTitle = 'Book plot number : ' + plotNumber;
+    this.getOwnersDetails(plotNumber);
   }
 
   handleOk(): void {
@@ -34,5 +75,54 @@ export class AppComponent implements OnInit {
   handleCancel(): void {
     console.log('Button cancel clicked!');
     this.isVisible = false;
+  }
+
+  submitForm(): void {
+    // tslint:disable-next-line: forin
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+    console.log(this.validateForm.controls);
+    let allCorrect = true;
+    for (const i in this.validateForm.controls) {
+      if (this.validateForm.controls[i].status === 'INVALID') {
+          allCorrect = false;
+      }
+    }
+    if (allCorrect) {
+      const customer: Customer = new Customer();
+      // tslint:disable-next-line: forin
+      for (const i in this.validateForm.controls) {
+        customer.id = this.plot;
+        customer.name = this.validateForm.get('name').value;
+        customer.email = this.validateForm.get('email').value;
+        customer.phoneNumber = this.validateForm.get('phoneNumber').value;
+      }
+      console.log(customer);
+      this.plotBookingService.addPerson(customer).subscribe((data) => {
+        data = +data;
+        if (data >= 1 && data <= 48) {
+          this.plotBookingService.getPeople().subscribe((data) => {
+            // tslint:disable-next-line: prefer-for-of
+            for (let i  = 0; i < data.length; i++) {
+              this.sites[data[i].id] = true;
+            }
+            this.createNotification('success');
+            this.validateForm.reset();
+            this.handleOk();
+          });
+        } else {
+          this.createNotification('error');
+        }
+      });
+    }
+  }
+
+  getOwnersDetails(plotNumber) {
+    this.plotBookingService.getOwner(plotNumber).subscribe((data: Customer) => {
+        this.owner = {...data};
+        console.log(this.owner);
+    });
   }
 }
--- PlotBooking/src/app/app.module.ts
+++ PlotBooking/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
 import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzNotificationModule } from 'ng-zorro-antd/notification';
 
 registerLocaleData(en);
 
@@ -22,7 +25,11 @@ registerLocaleData(en);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NzModalModule
+    NzModalModule,
+    NzFormModule,
+    NzIconModule,
+    ReactiveFormsModule,
+    NzNotificationModule
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
